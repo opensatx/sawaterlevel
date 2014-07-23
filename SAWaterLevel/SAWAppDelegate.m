277358dd0250f7c8bd7ff734bb3a34c2d5a9b3ca
Wayne <waynehartman@gmail.com>
2014-07-22 20:01:41 -0500

    Updated the background fetch to update irrigation schedule notifications.

--- SAWaterLevel/SAWAppDelegate.m
+++ SAWaterLevel/SAWAppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "SAWAppDelegate.h"
 #import "SAWNetworkController.h"
+#import "SAWDataController.h"
+#import "SAWStageLevel.h"
+#import "SAWLocalNotificationManager.h"
 
 @implementation SAWAppDelegate
 
@@ -18,12 +21,34 @@
 }
 
 - (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    SAWDataController *dataController = [[SAWDataController alloc] init];
+
+    NSString *houseNumber = [dataController fetchCachedHouseNumber];
+    SAWWaterLevel *existingWaterLevel = [dataController fetchCachedWaterLevel];
+
     SAWNetworkController *networkController = [SAWNetworkController sharedNetworkController];
     [networkController performBackgroundWaterLevelFetchWithCompletion:^(SAWWaterLevel *waterLevel, NSError *error) {
         if (error) {
             completionHandler(UIBackgroundFetchResultFailed);
         } else {
-            completionHandler(UIBackgroundFetchResultNewData);
+            //  We may need to slightly delay executing the background fetch completion in cases where we
+            //  need to update notifications
+            void(^executeCompletion)(void) = ^{
+                completionHandler(UIBackgroundFetchResultNewData);
+            };
+            
+            //  Update irrigation notifications if there is change in stage level
+            if (existingWaterLevel &&   //  got to have a previous water level
+                houseNumber &&          //  need to have the house number of the user
+                existingWaterLevel.stageLevel.level != waterLevel.stageLevel.level) //  and there needs to be a change in the stage level
+            {
+                SAWLocalNotificationManager *notificationManager = [SAWLocalNotificationManager localNotificationManager];
+                [notificationManager scheduleNotificationsForStageLevel:waterLevel.stageLevel streetNumber:houseNumber completion:^(NSArray *notifications) {
+                    executeCompletion();
+                }];
+            } else {    //  Nothing changed, so we're done
+                executeCompletion();
+            }
         }
     }];
 }
